4ca5a4d31b7619afe3b1006d8f019580b7e63cc7
Tuukka Heikura <theikura@iki.fi>
2020-02-09 16:29:47 +0200

    Add GraphQL operation to fetch messages from backend.

--- uifront/src/app/app.component.ts
+++ uifront/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import gql from 'graphql-tag';
+import {Apollo} from 'apollo-angular';
+
+const MsgQuery = gql`{messages {groupName userName postedAt msg}}`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'uifront';
+export class AppComponent implements OnInit {
+  title = 'chat';
+  msgs: any[];
+  loading = true;
+  error: any;
+  
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: MsgQuery,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.msgs = result.data && result.data.messages;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
+  }
 }
